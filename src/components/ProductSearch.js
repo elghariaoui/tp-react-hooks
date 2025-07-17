4b49e299cf90d8e5becf76efc29bc948f62c8cdc
elghariaoui <selghariaoui@gmail.com>
2025-07-17 23:08:43 +0200

    commit

--- src/components/ProductSearch.js
+++ src/components/ProductSearch.js
@@ -1,24 +1,34 @@
-import React, { useState, useContext } from 'react';
+import React, { useContext, useEffect, useState} from 'react';
 import { ThemeContext } from '../App';
+import {useDebounce} from '../hooks/useProductSearch';
+import { LanguageContext } from '../contexts/LanguageContext';
+const ProductSearch = ({ searchTerm, setSearchTerm }) => {
+    const { isDarkTheme } = useContext(ThemeContext);
 
-const ProductSearch = () => {
-  const [searchTerm, setSearchTerm] = useState('');
-  const { isDarkTheme } = useContext(ThemeContext);
-  // TODO: Exercice 2.1 - Utiliser le LanguageContext
-  
-  // TODO: Exercice 1.2 - Utiliser le hook useDebounce
-  
-  return (
-    <div className="mb-4">
-      <input
-        type="text"
-        value={searchTerm}
-        onChange={(e) => setSearchTerm(e.target.value)}
-        placeholder="Rechercher un produit..."
-        className={`form-control ${isDarkTheme ? 'bg-dark text-light' : ''}`}
-      />
-    </div>
-  );
+    const {language} = useContext(LanguageContext);
+    const placeholderText = {
+        fr:"Rechercher un produit...",
+        en:"Search for a product...",
+        de:"Suchen Sie nach einem Produkt",
+    };
+    const [inpVal, setInpVal] = useState(searchTerm);
+    const debouncedValue = useDebounce(inpVal, 2000);
+
+    useEffect(()=>{
+        setSearchTerm(debouncedValue);
+    }, [debouncedValue,setSearchTerm]);
+
+    return (
+        <div className="mb-4">
+            <input
+                type="text"
+                value={inpVal}
+                onChange={(e) => setInpVal(e.target.value)}
+                placeholder={placeholderText[language]}
+                className={`form-control ${isDarkTheme ? 'bg-dark text-light' : ''}`}
+            />
+        </div>
+    );
 };
 
 export default ProductSearch;
